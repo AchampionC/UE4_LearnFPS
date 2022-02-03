1567a2024c25ffac54fde5b3ba1f986bc2591cac
zhaodaxing <358957476@qq.com>
2022-02-03 11:12:35 +0800

    P11 对上一次增加的actor, 添加了UStaticMeshComp, USphereComponent 然后蓝图中关闭了UStaticMeshComp的Collision
    其中编写C++的时候, 注意#include<SphereComp> 要放在.cpp里而不是.h, 然后在头文件中添加前向class声明

--- Source/FPSGame/Public/FPSObjectiveActor.h
+++ Source/FPSGame/Public/FPSObjectiveActor.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "FPSObjectiveActor.generated.h"
 
+class USphereComponent;
+
 UCLASS()
 class FPSGAME_API AFPSObjectiveActor : public AActor
 {
@@ -16,6 +18,13 @@ public:
 	AFPSObjectiveActor();
 
 protected:
+
+	UPROPERTY(VisibleAnywhere, Category = "Components") // 希望在蓝图中更改组件的值
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* SphereComp;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
