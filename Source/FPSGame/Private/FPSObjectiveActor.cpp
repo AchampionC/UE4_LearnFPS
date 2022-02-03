9c00c29d2c7658c56b14cadb05705c6879d1c2e8
zhaodaxing <358957476@qq.com>
2022-02-03 12:54:36 +0800

    1. 从github 获得了视频中的uasset 2. 增加了Actor与Pawn重叠代码逻辑, 并且重叠时释放一定的特效, 重叠函数来自Actor, 然后overrride

--- Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -3,6 +3,7 @@
 
 #include "FPSObjectiveActor.h"
 #include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AFPSObjectiveActor::AFPSObjectiveActor()
@@ -11,10 +12,19 @@ AFPSObjectiveActor::AFPSObjectiveActor()
 	PrimaryActorTick.bCanEverTick = true;
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	RootComponent = MeshComp;
 
 	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
-	SphereComp->SetupAttachment(RootComponent);
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly); // 将碰撞只用于raycasts, sweeps, overlap, 这些查询通道
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore); // 忽略所有通道的碰撞
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap); // spherecomp的碰撞通道设置为 pawn,overlap
+ 	SphereComp->SetupAttachment(RootComponent);
+
+	// 需要在碰撞的时候触发PlayEffects
+	// 可以像以前一样addDynamic待补充
+	// 注意AddDynamic()绑定的函数，一定要像OnCollision(...) ...中参数对齐, 否则编译不通过
+	// SphereComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSObjectiveActor::OnCollision);
 }
 
 // Called when the game starts or when spawned
@@ -22,12 +32,30 @@ void AFPSObjectiveActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	PlayEffects();
 }
+
+void AFPSObjectiveActor::PlayEffects()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(this, PickupFX, GetActorLocation());
+}
+
+//void AFPSObjectiveActor::OnCollision(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+//{
+//	PlayEffects();
+//}
 
 // Called every frame
 void AFPSObjectiveActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	PlayEffects();
 }
 
--- Source/FPSGame/Public/FPSObjectiveActor.h
+++ Source/FPSGame/Public/FPSObjectiveActor.h
@@ -28,8 +28,17 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	//// 目标物体发生重叠时, 产生粒子效果
+	void PlayEffects();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	UParticleSystem* PickupFX;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override; // 因为需要重写这个函数, 所以加 “override”
+
+	//UFUNCTION()
+	//	void OnCollision(class UPrimitiveComponent* HitComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
